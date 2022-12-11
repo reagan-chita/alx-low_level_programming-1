a1be48360cccce93a8baba91547a4dffba64e102
luffyman <luffymonky12@gmail.com>
2022-12-11 13:07:06 -0800

    Salome

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,8 +14,8 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
        	{
-		printf("%d id positive\n", n);
-	} 
+		printf("%d is positive\n", n);
+	}
 	else if (n < 0)
 	{
 		printf("%d is negative\n", n);
