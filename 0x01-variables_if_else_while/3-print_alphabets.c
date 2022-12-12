c67106083e34e72de2e31d474617d6ba5322ae52
luffyman <luffymonky12@gmail.com>
2022-12-11 22:46:31 -0800

    print alp upper and lower case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet in lower and uper case
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp_u[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char alp_l[26] = "abcdefjhijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0 ; i < 26; i++)
+	{
+		putchar(alp_l[i]);
+	}
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp_u[i]);
+	}
+
+	putchar('\n');	
+	return (0);
+}
