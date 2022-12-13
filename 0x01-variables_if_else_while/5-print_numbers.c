b99c6ef54a7881da0b9a41591b97ff9193b4b7ba
luffyman <luffymonky12@gmail.com>
2022-12-12 16:27:13 -0800

    5-print_numbers.c file. It works!

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print base 10 numbers
+ *
+ *Return: 0 Always on success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+
+	return (0);
+}
