0f65d83e71437fe30d5f0402e13702b274f7faf7
luffyman <luffymonky12@gmail.com>
2022-12-12 16:55:48 -0800

    7-print_tebahpla.c file

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,18 @@
-#include
+#include <stdio.h>
+
+/**
+ * main - print alphabet in reverse
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 122; i > 96; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
