718bd74e47ef38bb8e78298d918fdcf03e9996cd
luffyman <luffymonky12@gmail.com>
2022-12-12 16:40:37 -0800

    6-print_numberz.c file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print base 10 numbers with new line
+ *
+ *Return: Alway 0 (Succedd)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar( i);
+	}
+	putchar('\n');
+	return (0);
+}
